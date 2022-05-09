5821a0bf0e1cc19ea50e3d59ae0259fa3b8dc78b
kensuke.saito@persol.co.jp <kensuke.saito@persol.co.jp>
2022-05-09 11:16:59 +0900

    Updates

---
+++ js/about.b0b7008d.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkpokedle_suggest"]=self["webpackChunkpokedle_suggest"]||[]).push([[443],{5087:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var s=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},u=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],l=n(1001),a={},r=(0,l.Z)(a,s,u,!1,null,null,null),c=r.exports}}]);
+//# sourceMappingURL=about.b0b7008d.js.map
